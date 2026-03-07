14e38b71de9a7bb8cd05b2f3a8ee3f663fb2036e
Anant Singh <YOUR_GITHUB_EMAIL>
2026-03-07 20:32:39 +0530

    Added UC11 Object Oriented Palindrome Service

---
+++ App/src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Object-Oriented Palindrome Checker");
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if (result)
+            System.out.println("The string is a Palindrome");
+        else
+            System.out.println("The string is NOT a Palindrome");
+
+        sc.close();
+    }
+}
