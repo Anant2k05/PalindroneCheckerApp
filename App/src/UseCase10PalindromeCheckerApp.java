1f0a328508c9f67189e67b9cc962108c2f2828c1
Anant Singh <YOUR_GITHUB_EMAIL>
2026-03-07 20:12:08 +0530

    Added UC10 Case Insensitive Palindrome Checker

---
+++ App/src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class UseCase10PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Case-Insensitive & Space-Ignored Palindrome Checker");
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        // Normalize the string
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(normalized))
+            System.out.println("The string is a Palindrome");
+        else
+            System.out.println("The string is NOT a Palindrome");
+
+        sc.close();
+    }
+}
